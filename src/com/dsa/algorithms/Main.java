57ba8445ab3735214b7e8f2637a868f52d9b776e
sumedhgole <spamsumedh@gmail.com>
2024-07-15 23:29:55 +0530

    created project structure, added LinkedList data structure

---
+++ src/com/dsa/algorithms/Main.java
@@ -0,0 +1,4 @@
+package com.dsa.algorithms;
+
+public class Main {
+}
---
+++ src/com/dsa/datastructures/linkedlists/Main.java
@@ -0,0 +1,25 @@
+package com.dsa.datastructures.linkedlists;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList linkedList = new LinkedList();
+
+
+        // insertion
+        linkedList.insertAtFront(2);
+        linkedList.insertAtFront(1);
+        linkedList.insertAtEnd_UsingTail(5);
+        linkedList.insertAtEnd(10);
+        linkedList.insertAtIndex(1, 22);
+
+        linkedList.printLinkedList();
+
+        // deletion
+        linkedList.deleteFromFront();
+
+        linkedList.printLinkedList();
+    }
+}
+
